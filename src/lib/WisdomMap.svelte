96b18438b82c84fb862cb5304608a3c96e474a3a
Botond Vasvári <botyware@gmail.com>
2023-06-24 16:48:28 +0200

    refact

--- src/lib/WisdomMap.svelte
+++ src/lib/WisdomMap.svelte
@@ -1,122 +1,143 @@
 <script>
-  import { Transformer } from 'markmap-lib';
-  import * as markmap from 'markmap-view';
-  import { onMount } from 'svelte';
-  import InfoWindow from './InfoWindow.svelte';
+import { Transformer } from 'markmap-lib';
+import * as markmap from 'markmap-view';
+import { onMount } from 'svelte';
+import InfoWindow from './InfoWindow.svelte';
 
-  //--------------------------------------------------
-  // props
-  //--------------------------------------------------
-  export let _ChosenTopic = "";
-  let _lastSelectedElem = null;
-  let _selectedSubTopic = "";
-  let _isInfoWindowVisible = false;
-  let _wisdomMapObject = null;
+//--------------------------------------------------
+// props
+//--------------------------------------------------
+export let _ChosenTopic = "";
+let _selectedElement = null;
+let _selectedSubTopic = "";
+let _isInfoWindowVisible = false;
+let _wisdomMapObject = null;
 
-  import markdownsWithOptions from '../../public/markdownWithOptions.js';
-  const markdownStr = markdownsWithOptions[_ChosenTopic].markdownStr; console.log(markdownStr); //todo get from api if app gets bigger//todo const markdownsWithOptions = getMarkdownWithOptions(_ChosenTopic);
-  const initialExpandLevel = Number(markdownsWithOptions[_ChosenTopic].initialExpandLevel);
-  const colorFreezeLevel = Number(markdownsWithOptions[_ChosenTopic].colorFreezeLevel);
+import markdownsWithOptions from '../../public/markdownWithOptions.js';
+const markdownStr = markdownsWithOptions[_ChosenTopic].markdownStr; console.log(markdownStr); //todo get from api if app gets bigger//todo const markdownsWithOptions = getMarkdownWithOptions(_ChosenTopic);
+const initialExpandLevel = Number(markdownsWithOptions[_ChosenTopic].initialExpandLevel);
+const colorFreezeLevel = Number(markdownsWithOptions[_ChosenTopic].colorFreezeLevel);
 
-  onMount(() => {    
-    window.addEventListener("click", selectSubTopic);
-    window.addEventListener("keyup", selectSubTopicWithArrows);
-    initWisdomMap(markdownStr, initialExpandLevel, colorFreezeLevel);
-  });
+onMount(() => {    
+  window.addEventListener("click", selectSubTopic);
+  window.addEventListener("keyup", selectSubTopicWithArrows);
+  initWisdomMap(markdownStr, initialExpandLevel, colorFreezeLevel);
+});
 
-  //--------------------------------------------------
-  // functions
-  //--------------------------------------------------
-  function initWisdomMap(p_markdownStr, p_initialExpandLevel, p_colorFreezeLevel) {
-    const transformer = new Transformer();
-        
-    // 0. transform markdown to data
-    const { root, features } = transformer.transform(p_markdownStr);
-    const { styles, scripts } = transformer.getUsedAssets(features);
-    const { Markmap, loadCSS, loadJS } = markmap;
+//--------------------------------------------------
+// functions
+//--------------------------------------------------
+function initWisdomMap(p_markdownStr, p_initialExpandLevel, p_colorFreezeLevel) {
+  const transformer = new Transformer();
+      
+  // 0. transform markdown to data
+  const { root, features } = transformer.transform(p_markdownStr);
+  const { styles, scripts } = transformer.getUsedAssets(features);
+  const { Markmap, loadCSS, loadJS } = markmap;
 
-    // 1. load assets
-    if (styles) loadCSS(styles);
-    if (scripts) loadJS(scripts, { getMarkmap: () => markmap });
+  // 1. load assets
+  if (styles) loadCSS(styles);
+  if (scripts) loadJS(scripts, { getMarkmap: () => markmap });
 
-    // 2. create markmap
-    const options = markmap.deriveOptions( {colorFreezeLevel: p_colorFreezeLevel, initialExpandLevel: p_initialExpandLevel} );
-    _wisdomMapObject = Markmap.create('#markmap', options, root);
-    console.log(_wisdomMapObject);
+  // 2. create markmap
+  const options = markmap.deriveOptions( {colorFreezeLevel: p_colorFreezeLevel, initialExpandLevel: p_initialExpandLevel} );
+  _wisdomMapObject = Markmap.create('#markmap', options, root);
+  console.log(_wisdomMapObject);
+}
+
+function selectSubTopic(event) {
+  const element = event.target;
+  if (!element.hasAttribute("xmlns")) return;
+
+  highlightTopic(element);
+  _selectedSubTopic = element.innerText;
+  _isInfoWindowVisible = false; _isInfoWindowVisible = true; // force rerender
+}
+
+function selectSubTopicWithArrows(event) {
+  const { key } = event;
+  if (key === "ArrowLeft" || key === "ArrowRight") {
+    if (!_selectedElement) return;
+    const currentDataPath = getCurrentDataPath();
+    const targetElement = calcTargetElementIfLeftOrRight(currentDataPath, event);
+    if (!targetElement) return;
+
+    highlightTopic(targetElement);
+    _selectedSubTopic = targetElement.innerText;
+  } else if (key === "ArrowDown" || key === "ArrowUp") {
+    if (!_selectedElement) return;
+    const currentDataPath = getCurrentDataPath();
+    const targetElement = calcTargetElementIfUpOrDown(currentDataPath, event);
+    if (!targetElement) return;
+
+    highlightTopic(targetElement);
+    _selectedSubTopic = targetElement.innerText;
+  }
+}
+
+function calcTargetElementIfLeftOrRight(currentDataPath, event) {
+  let targetDataPath;
+  switch (event.key) {
+    case "ArrowLeft":
+      targetDataPath = getParentNodeDataPathFrom(currentDataPath);
+      break;
+    case "ArrowRight":
+      targetDataPath = `${currentDataPath}.${Number(currentDataPath.split('.').pop()) + 1}`;
+      break;
   }
 
-  function highlightTopic(elem) {
-    if (_lastSelectedElem) {
-      _lastSelectedElem.classList.remove("active");
-    }
-    //// const underline = e.target.parentNode.parentNode.children[0];
-    _lastSelectedElem = elem;
-    _lastSelectedElem.classList.add("active");
+  const targetGroup = Array.from(document.getElementsByTagName('g'))
+    .find(el => el.getAttribute('data-path') === targetDataPath);
+
+  if (!targetGroup) return null;
+
+  const targetElement = Array.from(targetGroup.children)
+    .find(el => el.tagName === 'foreignObject')?.children[0];
+
+  return targetElement || null;
+}
+
+function calcTargetElementIfUpOrDown(currentDataPath, event) {
+  const sameParentNodes = Array.from(document.getElementsByTagName('g'))
+    .filter(el => getParentNodeDataPathFrom(el.getAttribute('data-path')) === getParentNodeDataPathFrom(currentDataPath));
+
+  const currentlySelectedIndex = sameParentNodes.findIndex(el => el.getAttribute('data-path') === currentDataPath);
+  const targetIndex = event.key === "ArrowUp" ? currentlySelectedIndex + 1 : currentlySelectedIndex - 1;
+  const totalNodes = sameParentNodes.length;
+
+  let targetNode;
+  if (targetIndex < 0) {
+    targetNode = sameParentNodes[totalNodes - 1];
+  } else if (targetIndex >= totalNodes) {
+    targetNode = sameParentNodes[0];
+  } else {
+    targetNode = sameParentNodes[targetIndex];
   }
 
-  function selectSubTopic(event) {
-    let elem = event.target;
-    if(!elem.hasAttribute("xmlns")) return;
-    highlightTopic(elem);
-    _selectedSubTopic = elem.innerText;
-    _isInfoWindowVisible = false; _isInfoWindowVisible = true; //forces rerender
+  const targetElement = Array.from(targetNode.children)
+    .find(el => el.tagName === 'foreignObject')?.children[0];
+
+  return targetElement || null;
+}
+
+function highlightTopic(element) {
+  if (_selectedElement) {
+    _selectedElement.classList.remove("active");
   }
 
-  function o(s) {
-    console.log(s);
-  }
+  _selectedElement = element;
+  _selectedElement.classList.add("active");
+}
 
-  function selectSubTopicWithArrows(e) {
-    if (e.key === "ArrowLeft" || e.key === "ArrowRight") {
-      if (!_lastSelectedElem) return;
-      let currentDataPath = getCurrentDataPath();
-      let targetElem = calcTargetElemIfLeftOrRight(currentDataPath, e);
-  // // o(targetElem);
-      if (!targetElem) return;
-      highlightTopic(targetElem);
-      _selectedSubTopic = targetElem.innerText;
-    }
-    else if(e.key === "ArrowDown" || e.key === "ArrowUp") {
-      if (!_lastSelectedElem) return;
-      let currentDataPath = getCurrentDataPath();
-      let targetElem = calcTargetElemIfUpOrDown(currentDataPath, e);
-  // // o(targetElem);
-      if (!targetElem) return;
-      highlightTopic(targetElem);
-      _selectedSubTopic = targetElem.innerText;
-    }
-  }
+function getCurrentDataPath() {
+  return _selectedElement.parentNode.parentNode.getAttribute("data-path");
+}
 
-  function getCurrentDataPath() {
-    return _lastSelectedElem.parentNode.parentNode.getAttribute("data-path");
-  }
+function getParentNodeDataPathFrom(currentDataPath) {
+  if (!currentDataPath) return null;
 
-  function calcTargetElemIfLeftOrRight(currentDataPath, event) {
-    let targetDataPath;
-    switch (event.key) {        
-      case "ArrowLeft":
-        targetDataPath = currentDataPath.split('.').slice(0, -1).join('.');//remove last element 
-        break;
-      case "ArrowRight":
-        targetDataPath = currentDataPath+'.'+(Number(currentDataPath.split('.').pop()) + 1);
-        break;
-    }
-    o(targetDataPath);
-    const temp = Array.from(document.getElementsByTagName('g')).filter(el => el.getAttribute('data-path') === targetDataPath)[0];
-    o(temp);
-    const targetElem = Array.from(temp.children).filter(el => el.tagName === 'foreignObject')[0].children[0];
-    return targetElem
-  }
-
-  function calcTargetElemIfUpOrDown(currentDataPath, event) {
-    //finds all elements with data-depth same as currently selected element
-    const elems = Array.from(document.getElementsByTagName('g')).filter(el => el.getAttribute('data-depth') === (currentDataPath.split('.').length - 1).toString());
-    //finds index of currently selected element
-    const index = elems.findIndex(el => el.getAttribute('data-path') === currentDataPath);
-    //returns data-path of element above or below
-    const parent =  event.key === "ArrowUp" ? elems[index + 1] : elems[index - 1];
-    return Array.from(parent.children).filter(el => el.tagName === 'foreignObject')[0].children[0];
-  }
+  return currentDataPath.split('.').slice(0, -1).join('.');
+}
 </script>
 <!------------------------------------------------------------------------------------------------------->
 <!------------------------------------------------------------------------------------------------------->
